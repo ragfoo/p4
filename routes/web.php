5b62d0c5f792e9f5fe065174cacfc046448d54c8
Ryan Gallagher <ryan_gallagher@apple.com>
2016-12-14 12:32:22 -0500

    getting the LensController set up

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/lenses', 'LensController@index')->name('lenses.index');
+Route::get('/lenses/create', 'LensController@create')->name('lenses.create');
+Route::post('/lenses', 'LensController@store')->name('lenses.store');
+Route::get('/lenses/{lens}', 'LensController@show')->name('lenses.show');
+Route::get('/lenses/{lens}/edit', 'LensController@edit')->name('lenses.edit');
+Route::put('/lenses/{lens}', 'LensController@update')->name('lenses.update');
+Route::delete('/lenses/{lens}', 'LensController@destroy')->name('lenses.destroy');
