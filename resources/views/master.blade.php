5ec008004f518ea75647f3181c1c17c84a0487ac
Ryan Gallagher <ryan_gallagher@apple.com>
2016-12-19 13:55:42 -0500

    update home page link

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -40,7 +40,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#">glasshub</a>
+                <a class="navbar-brand" href="/">glasshub</a>
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
